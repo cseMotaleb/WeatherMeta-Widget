5ac7c53dc8f570a4a772e33cfe7ee25b6d1fddfd
cseMotaleb <career.motaleb@gmail.com>
2024-12-28 12:43:24 +0600

    First Commit

---
+++ weathermate-widget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       WeatherMeta Widget
+ * Plugin URI:        https://github.com/cseMotaleb/RUL-Teams
+ * Description:       A plugin to manage team members with CRUD functionality.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Motaleb Hossain
+ * Author URI:        https://csemotaleb.github.io/cv/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       rul-teams
+ * Domain Path:       /languages
+ */
